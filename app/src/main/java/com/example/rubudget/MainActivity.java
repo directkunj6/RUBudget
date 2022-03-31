b3457ea417060b39f6020aba890b69641ca3db23
Ash4394 <ashusb94@gmail.com>
2022-03-31 16:08:38 -0400

    Register.java and Firebase integration

--- app/src/main/java/com/example/rubudget/MainActivity.java
+++ app/src/main/java/com/example/rubudget/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rubudget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
