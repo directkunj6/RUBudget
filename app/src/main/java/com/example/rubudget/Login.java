c2ae1712fea35bda0cee05b34b2a04815527d360
Ash4394 <ashusb94@gmail.com>
2022-04-01 15:09:45 -0400

    Added code for Login and a Logout button for the main screen.

--- app/src/main/java/com/example/rubudget/Login.java
+++ app/src/main/java/com/example/rubudget/Login.java
@@ -1,14 +1,71 @@
 package com.example.rubudget;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    EditText  mEmail, mPassword;
+    Button mLoginBtn;
+    FirebaseAuth fAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        mEmail = findViewById(R.id.email2);
+        mPassword = findViewById(R.id.password2);
+        mLoginBtn = findViewById(R.id.loginBtn);
+
+        fAuth = FirebaseAuth.getInstance();
+
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Email = mEmail.getText().toString().trim();
+                String Password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(Email)){
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(Password)){
+                    mPassword.setError("Password is Required");
+                    return;
+                }
+
+
+                fAuth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this,"Login is succesfull", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+
+                    }
+                });
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/rubudget/MainActivity.java
+++ app/src/main/java/com/example/rubudget/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.rubudget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mLogoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       mLogoutBtn = findViewById(R.id.LogoutBtn);
+
+       mLogoutBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startActivity(new Intent(getApplicationContext(),Login.class));
+
+           }
+       });
 
 
     }
