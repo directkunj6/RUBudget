8bfc0fefd13563fbe06f2f15e1372057cd12b2ff
directkunj6 <kpd66@scarletmail.rutgers.edu>
2022-04-02 01:11:10 -0400

    User Info Screen

---
+++ app/src/main/java/com/example/rubudget/userInfo.java
@@ -0,0 +1,14 @@
+package com.example.rubudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
