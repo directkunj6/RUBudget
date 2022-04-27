269d73edc02825808039b4d4c8e3d0d4bfc45ac0
directkunj6 <kpd66@scarletmail.rutgers.edu>
2022-04-26 23:29:36 -0400

    Token Screen adjustments

--- app/src/main/java/com/example/rubudget/userInfo.java
+++ app/src/main/java/com/example/rubudget/userInfo.java
@@ -15,28 +15,28 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class userInfo extends AppCompatActivity {
 
-    Button mSave_User_Income_Information;
-    String textIncomeInfo;
-    DatabaseReference mDatabase;
-    EditText mIncomeText;
-    Long incomeInfo;
+        Button mSave_User_Income_Information;
+        String textIncomeInfo;
+        DatabaseReference mDatabase;
+        EditText mIncomeText;
+        Long incomeInfo;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_info);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_user_info);
 
-        mIncomeText= findViewById(R.id.monthlyIncome);
-        mSave_User_Income_Information = findViewById(R.id.user_Income_Information);
-        mSave_User_Income_Information.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textIncomeInfo= mIncomeText.getText().toString().trim();
-                incomeInfo= Long.valueOf(textIncomeInfo);
-                String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                mDatabase = FirebaseDatabase.getInstance().getReference().child("Registered Users");
-                mDatabase.child(userId).child("Income").setValue(incomeInfo);
-            }
-        });
+            mIncomeText= findViewById(R.id.monthlyIncome);
+            mSave_User_Income_Information = findViewById(R.id.user_Income_Information);
+            mSave_User_Income_Information.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    textIncomeInfo= mIncomeText.getText().toString().trim();
+                    incomeInfo= Long.valueOf(textIncomeInfo);
+                    String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                    mDatabase = FirebaseDatabase.getInstance().getReference().child("Registered Users");
+                    mDatabase.child(userId).child("Income").setValue(incomeInfo);
+                }
+            });
+        }
     }
-}
